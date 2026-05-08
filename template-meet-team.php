a24e0a420949e8f7a8e4aaab6b3e14544b100caa
LAL Dev <dev@lal.local>
2026-05-08 05:38:25 -0700

    fix(qa): fix history floats, planner toolkit cards, meet-team PHP warnings
    history (template-history.php + _custom.scss):
    - Add .right-img-text-wrap / .left-img-text-wrap float rules to _custom.scss
    - These classes were used in template but had no CSS, causing images to stack
      instead of floating left/right alongside copy text
    
    planner toolkit (_subpages.scss):
    - Old .planner block used .card/.planner-heading/.card-img class names
    - Component (components-4-col-grid.php) emits .planner__card BEM names
    - Replace legacy block with .planner__card / .planner__card--heading /
      .planner__card--img BEM rules; add .meeting_planner_toolkit wrapper
    - Add min-height:175px + background-size:cover to .planner__card--img
    
    meet-the-team (template-meet-team.php):
    - Add null coalescing ?? '' to $team_section['team_sub_head'],
      $entry['alt_text'] (left and right column loops) to silence PHP warnings
      about undefined array keys when ACF data is missing or empty

--- template-meet-team.php
+++ template-meet-team.php
@@ -148,14 +148,14 @@ $team = get_field( "meet_the_team" );
 
 if ( $team ) {
     foreach ( $team as $team_section ) {
-        echo '<div class="container clearfix two-columns"><h2>' . $team_section['team_sub_head'] . '</h2>';
+        echo '<div class="container clearfix two-columns"><h2>' . ( $team_section['team_sub_head'] ?? '' ) . '</h2>';
         if ( $team_section['team_members'] ) {
             $team_members = $team_section['team_members'];
             foreach ( $team_members as $row ) {
                 echo '<div class="clearfix column left-column column-entry" style="margin-bottom: 35px;">';
                 foreach ( $row['left_column'] as $entry ) {
                     echo '<div class="team clearfix">';
-                    echo '<img class="team-img" style="width: 50%;" src="'. $entry['image'] .'" alt="' . $entry['alt_text'] . '" />';
+                    echo '<img class="team-img" style="width: 50%;" src="'. ( $entry['image'] ?? '' ) .'" alt="' . ( $entry['alt_text'] ?? '' ) . '" />';
                     echo '<h3 class="team-name">' . $entry['name'] . '</h3>';
                     echo '<div class="team-title">' . $entry['title'] . '</div>';
                     echo '<div class="team-phone">' . $entry['phone'] . '</div>';
@@ -167,7 +167,7 @@ if ( $team ) {
                 echo '<div class="clearfix column right-column column-entry " style="margin-bottom: 35px;">';
                 foreach ( $row['right_column'] as $entry ) {
                     echo '<div class="team clearfix">';
-                    echo '<img class="team-img" style="width: 50%" src="'. $entry['image'] .'" alt="' . $entry['alt_text'] . '" />';
+                    echo '<img class="team-img" style="width: 50%" src="'. ( $entry['image'] ?? '' ) .'" alt="' . ( $entry['alt_text'] ?? '' ) . '" />';
                     echo '<h3 class="team-name">' . $entry['name'] . '</h3>';
                     echo '<div class="team-title">' . $entry['title'] . '</div>';
                     echo '<div class="team-phone">' . $entry['phone'] . '</div>';
